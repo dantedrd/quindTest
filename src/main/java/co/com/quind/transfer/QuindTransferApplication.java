6e7c364c8ba7cf1fd6a0e3e661edb94c064dce9a
unknown <locodrd1@hotmail.com>
2024-02-25 02:34:47 -0500

    se cambia nombe a clase main

--- src/main/java/co/com/quind/transfer/QuindTransferApplication.java
+++ src/main/java/co/com/quind/transfer/QuindTransferApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdmininfoemployeesApplication {
+public class QuindTransferApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdmininfoemployeesApplication.class, args);
+		SpringApplication.run(QuindTransferApplication.class, args);
 	}
 
 }
